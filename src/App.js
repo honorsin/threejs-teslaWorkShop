decaf7c762174bb13923cc24343f75e007ed639d
honorsin <keshawncui@gmail.com>
2022-10-30 16:42:08 +0800

    shadow

--- src/App.js
+++ src/App.js
@@ -1,59 +1,79 @@
-import { useMemo, useRef } from 'react'
-import { Canvas, extend, useFrame, useThree } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { BufferAttribute, DoubleSide } from 'three'
+import { useMemo, useRef } from "react";
+import { Canvas, extend, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { BufferAttribute, DoubleSide } from "three";
 
-extend({ OrbitControls })
+extend({ OrbitControls });
 
 const Orbit = () => {
-  const { camera, gl } = useThree()
-  return <orbitControls args={[camera, gl.domElement]} />
-}
+  const { camera, gl } = useThree();
+  return <orbitControls args={[camera, gl.domElement]} />;
+};
 
 const Box = (props) => {
-  const ref = useRef()
+  const ref = useRef();
 
   useFrame(() => {
-    ref.current.rotation.x += 0.01
-    ref.current.rotation.y += 0.01
-  })
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
 
   return (
-    <mesh ref={ref} {...props}>
+    <mesh ref={ref} {...props} castShadow receiveShadow>
       <boxBufferGeometry />
       <meshBasicMaterial color="blue" />
     </mesh>
-  )
-}
+  );
+};
+
+const Floor = (props) => {
+  return (
+    <mesh {...props} receiveShadow>
+      <boxBufferGeometry args={[20, 1, 10]} />
+      <meshPhysicalMaterial />
+    </mesh>
+  );
+};
+
+const Bulb = (props) => {
+  return (
+    <mesh {...props}>
+      <pointLight castShadow />
+      <sphereBufferGeometry args={[0.2, 20, 20]}/>
+      <meshPhongMaterial emissive="yellow" />
+    </mesh>
+  );
+};
 
 const App = () => {
   const vertices = useMemo(
     () =>
       new BufferAttribute(new Float32Array([0, 0, 0, 0, 1, 1, 0, 1, -1]), 3),
     []
-  )
+  );
 
   const indices = useMemo(
     () => new BufferAttribute(new Uint16Array([0, 1, 2]), 1),
     []
-  )
+  );
 
   return (
-    <div style={{ height: '100vh', width: '100vw' }}>
-      <Canvas style={{ background: 'black' }} camera={{ position: [3, 3, 3] }}>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas
+        style={{ background: "black" }}
+        camera={{ position: [3, 3, 3] }}
+        shadows
+      >
         <Orbit />
-        <axesHelper args={[3]} />
+        <ambientLight intensity={0.2} />
+
+        <Bulb position={[0,3,0]}/>
+        <axesHelper args={[5]} />
         <Box position={[-1, 1, 2]} />
-        <mesh>
-          <bufferGeometry>
-            <bufferAttribute attach="attributes-position" {...vertices} />
-            <bufferAttribute attach="index" {...indices} />
-          </bufferGeometry>
-          <meshBasicMaterial side={DoubleSide} />
-        </mesh>
+        <Floor position={[0, -0.5, 0]} />
       </Canvas>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
