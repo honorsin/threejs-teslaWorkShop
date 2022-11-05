832ecb2872b8c3cb1e4b073e63a363f25349a223
honorsin <keshawncui@gmail.com>
2022-11-05 22:01:11 +0800

    boundingBox

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Background from './components/Background'
 import Draggable from './components/Draggable'
 import Floor from './components/Floor'
 import Model from './components/Model'
+import BoundingBox from './components/BoundingBox'
 import Box from './Box'
-
 const App = () => {
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
@@ -28,16 +28,27 @@ const App = () => {
           <Floor position={[0, -0.5, 0]} />
           <Draggable>
             <Bulb position={[0, 3, 0]} />
-            <Model
-              path="/tesla_model_3/scene.gltf"
-              position={[4, 0.6, 0]}
-              scale={[0.01, 0.01, 0.01]}
-            />
-            <Model
-              path="/tesla_model_S/scene.gltf"
-              position={[-4, 0.6, 0]}
-              scale={[0.8, 0.8, 0.8]}
-            />
+          </Draggable>
+          <Draggable transformGroup>
+            <BoundingBox
+              position={[4, 4, 0]}
+              dims={[3, 2, 6.1]}
+              offset={[0, -0.4, 0.75]}
+            >
+              <Model
+                path="/tesla_model_3/scene.gltf"
+                scale={[0.01, 0.01, 0.01]}
+              />
+            </BoundingBox>
+          </Draggable>
+          <Draggable transformGroup>
+            <BoundingBox
+              position={[-4, 4, 0]}
+              dims={[3, 2, 6.8]}
+              offset={[0, -0.8, 0.2]}
+            >
+              <Model path="/tesla_model_S/scene.gltf" scale={[0.8, 0.8, 0.8]} />
+            </BoundingBox>
           </Draggable>
         </Physics>
       </Canvas>
