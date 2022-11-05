38fd48ec74958beeccdb79268369bb9de685023f
honorsin <keshawncui@gmail.com>
2022-11-05 20:01:46 +0800

    texture2

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
   DoubleSide,
   TextureLoader,
   WebGLCubeRenderTarget,
+  Color
 } from "three";
 
 extend({ OrbitControls });
@@ -25,12 +26,38 @@ const Box = (props) => {
   const ref = useRef();
   const texture = useLoader(TextureLoader, "/wood.jpg");
   useFrame(() => {
+    ref.current.rotation.x += 0.01;
     ref.current.rotation.y += 0.01;
   });
 
+  const scaleUp = (mesh) => mesh.scale.set(1.5, 1.5, 1.5)
+
+  const scaleDown = (mesh) => mesh.scale.set(1, 1, 1)
+
+  const handlePointerDown = (e) => {
+    if (window.activeMesh) {
+      window.activeMesh.active = false
+      scaleDown(window.activeMesh)
+    }
+
+    e.object.active = true
+    scaleUp(e.object)
+    window.activeMesh = e.object
+  }
+
+  const handlePointerEnter = (e) => e.object.active || scaleUp(e.object)
+
+  const handlePointerLeave = (e) => e.object.active || scaleDown(e.object)
+   
+ 
   return (
-    <mesh ref={ref} {...props} castShadow>
-      <sphereBufferGeometry args={[1, 100, 100]}/>
+    <mesh ref={ref} {...props} 
+    castShadow 
+    onPointerDown={handlePointerDown}
+    onPointerEnter={handlePointerEnter}
+    onPointerLeave={handlePointerLeave}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
       <meshPhysicalMaterial map={texture} />
     </mesh>
   );
@@ -38,7 +65,7 @@ const Box = (props) => {
 
 const Background = (props) => {
   const texture = useLoader(TextureLoader, "/autoshop.jpg");
-  const {gl} = useThree();
+  const { gl } = useThree();
   const formatted = new WebGLCubeRenderTarget(
     texture.image.height
   ).fromEquirectangularTexture(gl, texture);
@@ -77,21 +104,38 @@ const App = () => {
     []
   );
 
+  const handleClick = e => {
+    if (!window.activeMesh) return
+    console.log(e)
+
+    window.activeMesh.material.color = new Color(e.target.style.backgroundColor)
+  }
+
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
+    <div style={{ height: "100vh", width: "100vw" }} >
+      <div style={{position: 'absolute', zIndex: 1}} >
+        <div style={{backgroundColor: 'blue', height: 50, width: 50}} onClick={handleClick}>
+        </div>
+        <div style={{backgroundColor: 'red', height: 50, width: 50}} onClick={handleClick}>
+        </div>
+        <div style={{backgroundColor: 'black', height: 50, width: 50}} onClick={handleClick}>
+        </div>
+      </div>
       <Canvas
         style={{ background: "black" }}
-        camera={{ position: [3, 3, 3] }}
+        camera={{ position: [7, 7, 7] }}
         shadows
       >
-
         <Orbit />
         <ambientLight intensity={0.2} />
 
         <Bulb position={[0, 3, 0]} />
         <axesHelper args={[5]} />
         <Suspense fallback={null}>
-          <Box position={[0, 1.5, 0]} />
+          <Box position={[-4, 1, 0]} />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Box position={[4, 1, 0]} />
         </Suspense>
         <Suspense fallback={null}>
           <Background />
